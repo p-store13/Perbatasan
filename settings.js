a0c8e4d314d6294556406b320bec9b486f75ec4b
Pabedilan <114459299+Pabedilan@users.noreply.github.com>
2023-07-31 19:21:39 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,70 @@
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+const version = require("baileys/package.json").version 
+global.require = require
+global.language = "id"
+global.nomerOwner ="6283148450932"// ganti sesuka hati lo
+global.nomerOwner2 = "6283148450932"// ganti sesuka hati lo
+global.runWith = "Replit"
+global.ownerName = "𝕳𝖊𝖓𝖉𝖗𝖆 𝕺𝖋𝖋𝖎𝖈𝖎𝖆𝖑"// ganti sesuka hati lo
+global.botName = "𝕻𝖊𝖗𝖇𝖆𝖙𝖆𝖘𝖆𝖓 𝕭𝖔𝖙𝖟" // ganti sesuka hati lo
+global.sessionName ="session" //untuk session rm- rf session
+global.setmenu ="document" // gak ush lo ganti
+global.docType = "docx" // ga ush di ganti
+global.Qoted = "ftoko" // ga ush di ganti
+global.autoBio = true // tanya lana kalo mau aktifin auto bio
+global.baileysMd = true
+global.antiSpam = true
+//global.multi = true
+//global.prefa = "!"
+global.fake = botName
+global.Console = false
+//global.autorespon = false
+global.copyright = `𝕳𝖊𝖓𝖉𝖗𝖆 𝕺𝖋𝖋𝖎𝖈𝖎𝖆𝖑`
+global.baileysVersion = `𝕻𝖊𝖗𝖇𝖆𝖙𝖆𝖘𝖆𝖓 𝕭𝖔𝖙𝖟 V2`
+//global.On = "On"
+//global.Off ="Off"
+//global.autoblockcmd = false
+global.fake1 ="𝕻𝖊𝖗𝖇𝖆𝖙𝖆𝖘𝖆𝖓 𝕭𝖔𝖙𝖟"
+global.packName = "𝕻𝖊𝖗𝖇𝖆𝖙𝖆𝖘𝖆𝖓 𝕭𝖔𝖙𝖟"
+global.authorName = "𝕳𝖊𝖓𝖉𝖗𝖆 𝕺𝖋𝖋𝖎𝖈𝖎𝖆𝖑"
+global.replyType = "web"
+global.setwelcome = "type11"
+global.autoblockcmd = false
+global.autoReport = true
+global.autoLevel = true
+global.autoSticker = false // true untuk kirim foto otomatis jd stiker
+global.gamewaktu = 60
+global.limitCount = 30 // untuk limit user
+global.Intervalmsg = 1000 //detik
+global.fileStackApi ="Amje6cHl6QWm1tHLLQbX7z" //daftar di filestack
+global.apiflash ="9b9e84dfc18746d4a19d3afe109e9ea4"
+global.apikey ="ReiiXLucifer"
+global.rosekey ="Rs-Reii_code"
+global.dankey ="ReiiCode"
+// beli sendiri / daftar di website https://api.lolhuman.xyz/
+
+// make apikey lolhuman // BELI AJ api ny , gw ga nyediaain apikey
+
+//global.anonymousImg = "https://telegra.ph/file/1446ae19e40a769e94248.jpg"
+//global.botImg = "https://telegra.ph/file/67f456acc78d2252dde2f.jpg"
+//global.fakegifImg ="https://telegra.ph/file/b4ec766d46519110784c3.jpg"
+//global.quotedImg = "https://telegra.ph/file/037f85737e981c7158d3d.jpg"
+//global.replyImg = "https://telegra.ph/file/861077ba89491ed8cc6f1.jpg"
+//global.thumbImg ="https://telegra.ph/file/2b5e7da186256332c9dc8.jpg"
+//global.thumbnaildokumenImg = "https://telegra.ph/file/67f88bf1209262d81ffe8.jpg"
+//global.videoMp4 = "https://telegra.ph/file/fcc8a8d9ea62088fb9f4b.mp4"
+//global.webImg = "https://telegra.ph/file/f7da244c6cbf47f55f59b.jpg"
+global.gcounti = {
+'prem' : 60, //limit prem
+'user' : 20 //limit
+} 
+
+// Maulana Bot & Lana Botz hanya 1 Orang
+// SCRIPT BY : LANA DEV & Dittaz
+// EROR?? CHAT GROUP : https://chat.whatsapp.com/JgJHpvM7K6UAYVlT2zQPFA
+// JANGAN DI HAPUS Y
+// JUAL SCRIPT?? NERAKA PALING BAWAH
+
+
+//===================//
